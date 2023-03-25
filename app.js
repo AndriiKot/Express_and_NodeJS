cbb20e5995866135f718e02201cfa2a4b5edf281
AndriiKot <a_kocyuba@ukr.net>
2023-03-25 10:45:27 +0100

    Argv arguments

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-﻿
+﻿//console.log(process);
+// node app one 2 3
+
+const arg = process.argv;
+console.log(arg);
+let a,b;
+a = arg[2];
+b = arg[3];
+
+if (a > b){
+  console.log(a);
+} else {
+  console.log(b);
+}
+
+ 
