98ce0093a4fa84078d5b1a552092982cde0d3f84
AndriiKot <a_kocyuba@ukr.net>
2023-03-25 10:58:47 +0100

    Argv typeof

--- app.js
+++ app.js
@@ -7,10 +7,35 @@ let a,b;
 a = arg[2];
 b = arg[3];
 
+function c(a,b) {
 if (a > b){
   console.log(a);
 } else {
   console.log(b);
 }
+}
+
+console.log(c(a,b));
+
+
+// node app 4 1
+console.log(c(a,b));      // 4         
+
+// node app 8 555
+
+c(a,b);     // 8
+
+console.log(typeof a);  // string
+console.log(typeof b);  // string
+
+
+// node app 8 555
+a = +a
+b = +b
+c(a,b)  // 555
+
+
+
+
 
  
